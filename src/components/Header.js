b3b2480f1bcd6c1b06808c8e63a4edc8a5d902ba
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-30 17:00:49 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -81,7 +81,7 @@ const Header = ({ isLoginState, isLoginHandler }) => {
             onClick={goMainPage}
             style={{ marginLeft: 120, marginTop: 10, marginBottom: 10 }}
           >
-            WOORI ARTE
+            blue / green
           </text>
           {/* 버튼 하나로 변경하기  */}
 
